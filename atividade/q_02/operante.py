ad982d695009775fb67b2a82ee869b0b28ffc46d
Edu-tentando-49 <pudimcomcaramelo49@gmail.com>
2021-09-03 19:13:43 -0300

    já havia feito a atividade anteriormente, no entanto, foi feito a diferenciaçãio de nomes para melhor documentação

---
+++ atividade/q_02/operante.py
@@ -0,0 +1,9 @@
+class somar:
+    @staticmethod
+    def adiciona(vet_recebe):
+        resultado = 0
+
+        for algo in vet_recebe:
+            resultado = resultado + algo
+
+        return resultado
---
+++ atividade/q_02/principal.py
@@ -0,0 +1,6 @@
+from operante import somar
+
+vet_recebe = [1, 2]
+
+resultado = somar.adiciona(vet_recebe)
+print("soma do único valor no vetor é: " + str(resultado))
